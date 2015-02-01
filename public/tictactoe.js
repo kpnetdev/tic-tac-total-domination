3dc2358bf99214a08b6f443127d4fea33309ed12
kpnetdev <kelley.puckett@gmail.com>
2015-01-31 19:18:32 -0800

    adds jquery and tictactoe.js to public, fiddles with css

---
+++ public/tictactoe.js
@@ -0,0 +1,88 @@
+function fetchBoard() {
+  "use strict";
+  var board;
+  board = ['', '', '', '', '', '', '', '', ''];
+  $("#tictac td").each(function (index) {
+    board[index] = $(this).text();
+  });
+  return board;
+}
+
+function showGameOver(result) {
+  "use strict";
+  var target;
+  target = $("#result");
+  if (result > 0) {
+    target.css('color', '#800');
+    target.text("You win!");
+  } else if (result < 0) {
+    target.css('color', '#008');
+    target.text("I win!");
+  } else {
+    target.css('color', '#505');
+    target.text("Tie game.");
+  }
+}
+
+function resetGame() {
+  "use strict";
+  var target;
+
+  $("#tictac td").text('');
+  target = $("#result");
+  target.css('color', '#000');
+  target.text('Click a square');
+}
+
+function moveAt() {
+  "use strict";
+  var playerSquare, board, result, oLocation, oCell;
+
+  playerSquare = $(this);
+
+  // return if square is already full or if game is over !!!CHANGE GAME OVER CHECK!!!
+  if (playerSquare.text() !== '' || checkWin(fetchBoard()) !== 0) {
+    return;
+  }
+
+  // place 'X' at selected location
+  playerSquare.css('color', '#800');
+  playerSquare.text('X');
+
+  // if game is over, display message
+  board = fetchBoard();
+  result = checkWin(board);
+  if (result !== 0) {
+    showGameOver(result);
+    return;
+  }
+
+  // find where to place the 'O'
+  oLocation = selectMove(board);
+  if (oLocation < 0) {
+    // if there is no valid place, it is tie game
+    showGameOver(0);
+    return;
+  }
+
+  // place 'O' at location
+  board[oLocation] = 'O';
+  oCell = $("#cell" + oLocation);
+  oCell.css('color', '#008');
+  oCell.text('O');
+
+  // if game is over, display that
+  result = checkWin(board);
+  if (result !== 0) {
+    showGameOver(result);
+    return;
+  }
+}
+
+$(document).ready(function () {
+  "use strict";
+
+  //$("#tictac td").click(moveAt);
+  $("#tictacreset").click(resetGame);
+  resetGame();
+});
